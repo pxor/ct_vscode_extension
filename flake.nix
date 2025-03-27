f2297ddeee5f58441387a495d59d11c22a4d7b02
pxor <nedislav@gmail.com>
2025-03-27 17:04:47 +0200

    feat: Add nix build

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Dev environment for ct_vscode VS Code extension";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;  # Needed for vscode
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_18
+            yarn
+            nodePackages.typescript
+            nodePackages.eslint
+          ];
+
+          shellHook = ''
+            echo "VS Code Extension Dev Shell Ready 🚀"
+          '';
+        };
+      });
+}
---
+++ nix/shells/main.nix
@@ -0,0 +1,1 @@
+(import ./flake.nix).outputs.devShell.x86_64-linux
